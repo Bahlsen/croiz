14bb284858acdd026c6a8ff36cd9e72fb20db53b
CroizDev <dev@croiz.local>
2025-12-07 17:46:11 -0500

    fix(main): update main function to support asynchronous initialization and set preferred device orientation

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:croiz/routes/app_router.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const ProviderScope(child: CroizApp()));
 }
 
